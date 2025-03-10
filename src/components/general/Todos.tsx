00556d03a704a9c5ec02a72c5f558d3e56576530
GrokFan <developer.umarbek@gmail.com>
2025-03-10 15:21:17 +0600

    update TodoTable

--- src/components/general/Todos.tsx
+++ src/components/general/Todos.tsx
@@ -1,33 +1,17 @@
-import {
-  Table,
-  TableBody,
-  TableCaption,
-  TableCell,
-  TableHead,
-  TableHeader,
-  TableRow,
-} from "@/components/ui/table";
 import { Button } from "../ui/button";
 
 export function TodoTable({ todos }: { todos: ITodo[] }) {
   return (
-    <Table className="w-3xl">
-      <TableCaption>Все листы</TableCaption>
-      <TableHeader>
-        <TableRow>
-          <TableHead className="w-[100px]">Название</TableHead>
-        </TableRow>
-      </TableHeader>
-      <TableBody>
-        {todos.map((todo) => (
-          <TableRow key={todo.id}>
-            <TableCell className="font-medium">{todo.title}</TableCell>
-            <div className="ml-auto">
-              <Button>Изменить</Button>
-            </div>
-          </TableRow>
-        ))}
-      </TableBody>
-    </Table>
+    <div className="w-full flex flex-col gap-2">
+      {todos.map((todo) => (
+        <div key={todo.id} className="w-full flex items-center border-b-2 py-2">
+          <p className="font-medium">{todo.title}</p>
+          <div className="ml-auto flex gap-2">
+            <Button variant="outline">Изменить</Button>
+            <Button variant="secondary">Удалить</Button>
+          </div>
+        </div>
+      ))}
+    </div>
   );
 }
